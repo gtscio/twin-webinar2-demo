fca571b585831d4b4fb3e318f34b989b613b24e4
Jose M. Cantera <jose.cantera@iota.org>
2025-09-16 16:05:53 +0200

    next snapshot

---
+++ scripts/issue-compliance-credential-data-resource.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ "$#" -ne 2 ]; then echo "Please provide a data resource name and data resource ID URI"
+    exit -1
+fi
+
+docker run -it -v "$(pwd)/../dataset:/ext" --rm onboardingcli/onboardingcli vc create \
+--claims-file "/ext/claims/data-resources/$1-data-resource-compliant.json" \
+--template-file /ext/templates/compliance-credential-template.json \
+--subject-did $2 --trusted-issuer-file /ext/identities/clearing-house/clearing-house.json \
+--vc-version 2 --issuers-dir /ext/identities --templates-dir /ext/templates --vc-dir /ext/credentials/compliance \
+--valid-for 20000h
--- scripts/issue-data-resource-credential.sh
+++ scripts/issue-data-resource-credential.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+#!/bin/sh
+if [ "$#" -ne 3 ]; then echo "Please provide a participant name, a data resource name and ID URL"
+    exit -1
+fi
+
 docker run -it -v "$(pwd)/../dataset:/ext" --rm onboardingcli/onboardingcli vc create \
---claims-file /ext/claims/documents-data-resource.json --template-file /ext/templates/template.json \
---trusted-issuer-file /ext/identities/acme/acme.json --vc-version 2 \
---subject-did "https://my-data-resources.example.org/docs-resource-1" \
---issuers-dir /ext/identities --templates-dir /ext/templates --vc-dir /ext/credentials/evidences \
+--claims-file "/ext/claims/data-resources/$2-data-resource.json" --template-file /ext/templates/template.json \
+--trusted-issuer-file "/ext/identities/$1/$1.json" --vc-version 2 \
+--issuers-dir /ext/identities --templates-dir /ext/templates --vc-dir /ext/credentials/evidences/data-resources \
+--subject-did $3 \
 --valid-for 20000h
